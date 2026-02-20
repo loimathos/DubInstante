ac2bdf36da17d6206002604442228adf675a9683
loimathos <loimathos616@gmail.com>
2026-02-20 15:53:25 +0100

    feat: Add Android port including a QML-based phone GUI, local APK build automation, and CI pipeline, with corresponding documentation and build system updates.

--- src/phonegui/main.cpp
+++ src/phonegui/main.cpp
@@ -1,17 +1,24 @@
+#include <QCoreApplication> // Added for QCoreApplication::exit
+#include <QDebug>           // Added for qCritical()
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QUrl>
+
+using namespace Qt::StringLiterals;
 
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
   app.setApplicationName("DubInstante");
 
   QQmlApplicationEngine engine;
-  const QUrl url(QStringLiteral("qrc:/Main.qml"));
+  const QUrl url(u"qrc:/qt/qml/DubInstante/Main.qml"_s);
   QObject::connect(
       &engine, &QQmlApplicationEngine::objectCreated, &app,
       [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
+        if (!obj && url == objUrl) {
+          qCritical() << "Failed to load Main.qml from" << url;
           QCoreApplication::exit(-1);
+        }
       },
       Qt::QueuedConnection);
   engine.load(url);
